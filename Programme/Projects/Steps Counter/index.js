e51ab275b989c63378b8d24970d17b76a6a2a00e
Shubham Singh <shubhamsinghcodex@gmail.com>
2021-06-30 18:38:39 +0530

    idk

--- Programme/Projects/Steps Counter/index.js
+++ Programme/Projects/Steps Counter/index.js
@@ -1,9 +1,18 @@
 const stp = document.querySelector(".steps");
-const pcl = document.querySelector(".container p");
+const pcl = document.querySelector(".cm");
+const pre = document.querySelector(".rm");
+var mul = document.querySelector(".inp");
 var i = 0;
 
 pcl.addEventListener("click", () => {
     i++;
-    stp.innerHTML = (i*14).toString();
+    stp.innerHTML = (i*(mul.value)).toString();
     pcl.innerHTML =  i.toString() +  " times Clicked";
-})
+});
+
+pre.addEventListener("click", () => {
+    i = 0;
+    mul.value = "";
+    stp.innerHTML = "0";
+    pcl.innerHTML =  "Click Me";
+});
--- Programme/Projects/Steps Counter/index.php
+++ Programme/Projects/Steps Counter/index.php
@@ -9,10 +9,17 @@
     <title>Steps Counter</title>
   </head>
   <body>
-      <div class="container">
-          <h1>Total Steps are:<br><span class="steps"></span></h1>
-          <p> Click Me </p>
+    <div class="hm">
+      <h2>One Click equals:</h2>
+      <input class="inp" type="text" placeholder="Enter your Value here"/>
+    </div>
+    <div class="container">
+      <h1>Total Steps are:<br /><span class="steps"></span></h1>
+      <div class="btn">
+        <p class="cm">Click Me</p>
+        <p class="rm">Reset</p>
       </div>
+    </div>
     <script src="index.js"></script>
   </body>
 </html>
